9d87036ed9bc2be5ac96cc545604884b977f0354
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-05-27 23:54:41 +0300

    add category models

--- andypiix/models.py
+++ andypiix/models.py
@@ -12,3 +12,15 @@ class Image(models.Model):
     img_category = models.ForeignKey(Category,on_delete=models.CASCADE)
     img_location = models.ForeignKey(Location,on_delete=models.CASCADE)
     
+    def __str__(self):
+        return self.image_name
+    
+
+class Category(models.Model):
+    '''
+    create a  Category model
+    '''
+    category = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.category
