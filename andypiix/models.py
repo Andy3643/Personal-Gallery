02c02dd6083e80bf6d1410ccdb0ca7a2259f9ac2
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-05-27 23:52:46 +0300

    ft:create image class model

--- andypiix/models.py
+++ andypiix/models.py
@@ -1,3 +1,14 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    """
+    create an image model
+    """
+    image = models.ImageField(upload_to='andypiix/')
+    img_name = models.CharField(max_length=40)
+    img_description = models.TextField()
+    img_category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    img_location = models.ForeignKey(Location,on_delete=models.CASCADE)
+    
--- gallery/settings.py
+++ gallery/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'andypiix',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
