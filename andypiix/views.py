9da694c15f9512f44073373f6e072f3f43480cb6
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-05-30 00:26:45 +0300

    ft:query images from the database

--- andypiix/views.py
+++ andypiix/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Image
 # Create your views here.
 def index(request):
-    
+    image = Image.objects.all()
     return render (request,'index.html')
