011cd845890940b19d8dca5270ac7c06e6497b35
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-05-30 08:06:20 +0300

    ft:add search method to display searched images

--- andypiix/views.py
+++ andypiix/views.py
@@ -4,3 +4,21 @@ from .models import Image
 def index(request):
     image = Image.objects.all()
     return render (request,'index.html', {'image':image})
+
+def search_images (request):
+    """
+    method to display users search results
+    """
+    if 'images' in request.GET and request.GET['images']:
+        searched_category = request.GET.get('images')
+        searched_images = Image.search_image_by_category(searched_category)
+        message = f'{searched_category}'
+        context = {
+            "searched_images":searched_images,
+            "message":message
+        }
+        return render(request,'search.html',context)
+
+    else:
+        message = "Input a relevant search"
+        return render(request,'search.html',{"message":message})
